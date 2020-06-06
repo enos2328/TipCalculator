abe412da56a052787a0a7c6407428566428f4337
Athena Enosara <aenosara@csumb.edu>
2020-06-06 07:01:30 -0700

    Stuff

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -12,31 +12,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UITextField!
     @IBOutlet weak var totalLabel: UITextField!
+    @IBOutlet weak var tipPercentageField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
     @IBAction func onTap(_ sender: Any) {
         view.endEditing(true)
     }
     
     @IBAction func calculateTip(_ sender: Any) {
-        //GET BILL AMOUNT
-        let bill = Double(billField.text!) ?? 0 //let is a constant // ?? 0 says that anything that isn't a number gets changed to zero
+        //GET BILL AND TIP AMOUNT
+        let bill = Double(billField.text!) ?? 0
+        let tipPercentage = tipPercentageField.text as! Int
         
         //CALCULATE THE TIP AND TOTAL
-        let tip = bill * 0.1
+        let tip = bill * (tipPercentage * 0.01)
         let total = bill + tip
-        
+                
         //UPDATE THE TIP AND TOTAL LABELS
-//        tipLabel.text = "$\(tip)" // the \() separates text from variable
-        tipLabel.text = String(format: "$%.2f", tip) // the %f gets replaced by the tip variable, the .2 sets 2 decimal places
-        
-//        totalLabel.text = "$\(total)"
-        tipLabel.text = String(format: "$%.2f", total) // the %f gets replaced by the tip variable
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
     }
+    
+    
 }
 
